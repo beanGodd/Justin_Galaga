a23389d6ab9a55521809469d20d30b8bcea696e7
beanGodd <49986882+beanGodd@users.noreply.github.com>
2019-04-26 08:50:42 -0500

    Add files via upload

---
+++ com/cctc/amatlock/test/KeyInput.java
@@ -0,0 +1,42 @@
+package com.cctc.amatlock.test;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyInput implements KeyListener
+{
+    @Override
+    public void keyTyped(KeyEvent e)
+    {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e)
+    {
+        Spaceship ship = Screen.getInstance().ship;
+
+        if(e.getKeyCode() == KeyEvent.VK_LEFT)
+        {
+            ship.setVelX(-2);
+        }
+        else if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+        {
+            ship.setVelX(2);
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e)
+    {
+       Spaceship ship = Screen.getInstance().ship;
+        if(e.getKeyCode() == KeyEvent.VK_RIGHT)
+        {
+            ship.setVelX(0);
+        }
+        else if (e.getKeyCode() == KeyEvent.VK_LEFT)
+        {
+            ship.setVelX(0);
+        }
+    }
+}
---
+++ com/cctc/amatlock/test/Spaceship.java
@@ -0,0 +1,46 @@
+package com.cctc.amatlock.test;
+
+import java.awt.*;
+
+public class Spaceship extends CoreObject
+{
+      public int score;
+    /**
+     * Creates the core object. All subclasses
+     * will call this with super.
+     * The super call to the Rectangle class.
+     *
+     * @param x
+     * @param y
+     * @param width
+     * @param height
+     * @param color
+     */
+    public Spaceship(int x, int y, int width, int height, Color color)
+    {
+        super(x, y, width, height, color);
+    }
+
+    @Override
+    public void tick()
+    {
+        x += velX;
+        int counter = 0;
+        counter++;
+        if(counter == 30)
+        {
+            shoot();
+        }
+    }
+    @Override
+    public void render(Graphics g)
+    {
+        g.setColor(color);
+        g.fillRect(x,y,width,height);
+    }
+    public void shoot()
+    {
+        Laser laser = new laser(x, y, 10, 30, Color.WHITE);
+        Screen.addObject(laser);
+    }
+}
